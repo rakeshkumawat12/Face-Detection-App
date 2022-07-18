3ca3eca8dba75bce16bbf8994f55766e319467a0
Rakesh Kumawat <rakeshkumawatrrkk12877@gmail.com>
2022-07-18 21:47:15 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import NewPost from "./components/NewPost";
+import face from "./assets/face.png"
 
 function App() {
+  const [file, setFile] = useState();
+  const [image, setImage] = useState();
+
+  useEffect(() => {
+    const getImage = () => {
+      const img = new Image();
+      img.src = URL.createObjectURL(file);
+      img.onload = () => {
+        setImage({
+          url: img.src,
+          width: img.width,
+          height: img.height,
+        });
+      };
+    };
+
+    file && getImage();
+  }, [file]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {image ? (
+        <NewPost image={image} />
+      ) : (
+        <div className="newPostCard">
+          <div className="addPost">
+            <img
+              src={face}
+              alt=""
+              className="avatar"
+            />
+            <div className="postForm">
+              <label htmlFor="file">
+                <img
+                  className="addImg"
+                  src="https://cdn.icon-icons.com/icons2/564/PNG/512/Add_Image_icon-icons.com_54218.png"
+                  alt=""
+                />
+                <button>Send</button>
+              </label>
+              <input
+                onChange={(e) => setFile(e.target.files[0])}
+                id="file"
+                style={{ display: "none" }}
+                type="file"
+              />
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
